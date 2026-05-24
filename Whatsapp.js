0dae4b1d2bf8ffb05a5267d7589af9be2c95e2ee
Ryan Henrry <ryanhenrry72@gmail.com>
2026-05-24 18:23:32 -0300

    Add files via upload

--- Whatsapp.js
+++ Whatsapp.js
@@ -1,3 +1,3 @@
 document.getElementById("whatsappBtn").addEventListener("click", function() {
-    window.open("https://wa.me/5535999999999", "_blank");
+    window.open("https://wa.me/5511973869918", "_blank");
 });
